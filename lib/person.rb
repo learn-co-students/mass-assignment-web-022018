00609372c4cb4587a8654ab512db6193cfba66c7
github username <github email address>
2018-03-02 11:41:32 -0500

    Wrote person.rb

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-  #your code here
+
+  @@keys = []
+
+  def initialize(argument)
+    @@keys << argument.keys
+    self.class.accessors
+    argument.each {|k,v| self.send("#{k}=", v)}
+  end
+
+  def self.accessors
+    @@keys.last.each {|k| attr_accessor k.to_sym}
+  end
+
 end
